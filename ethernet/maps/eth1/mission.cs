b8b096f626a41e32f5f66110c9e3235125147a53
unknown <mike@.(none)>
2009-12-15 23:42:12 +0100

    Add material mappings to eth1.

--- ethernet/maps/eth1/mission.cs
+++ ethernet/maps/eth1/mission.cs
@@ -7,8 +7,26 @@ $sgLightEditor::filterDBPath = $MAP_ROOT @ "filters/";
 sgLoadDataBlocks($sgLightEditor::lightDBPath);
 sgLoadDataBlocks($sgLightEditor::filterDBPath);
 
-//exec("./difs/propertymap.cs");
-//exec("./scripts/env.cs");
+//------------------------------------------------------------------------------
+// Material mappings
+//------------------------------------------------------------------------------
+
+%mapping = createMaterialMapping("gray3");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("grass2");
+%mapping.sound = $MaterialMapping::Sound::Soft;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("rock");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("stone");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
 
 
 
