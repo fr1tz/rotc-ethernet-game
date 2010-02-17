2136274e888b2e7a22a7e6dbe9866898ba8cc68e
Michael Goldener <mg@wasted.ch>
2010-02-17 23:38:36 +0100

    Increase assault rifle splash damage from 25 to 35.

--- ethernet/server/scripts/weapons/assaultrifle/assaultrifle.cs
+++ ethernet/server/scripts/weapons/assaultrifle/assaultrifle.cs
@@ -35,7 +35,7 @@ datablock TracerProjectileData(RedAssaultRifleProjectile)
 	// script damage properties...
 	impactDamage        = 0;
 	impactImpulse       = 1000;
-	splashDamage        = 25;
+	splashDamage        = 35;
 	splashDamageRadius  = 5;
 	splashDamageFalloff = $SplashDamageFalloff::Exponential;
 	splashImpulse       = 0;
