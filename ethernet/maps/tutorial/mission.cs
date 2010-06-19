08df4f521d9684e73ad5af73656fecae1897a6fd
Michael Goldener <mg@wasted.ch>
2010-06-19 04:45:28 +0200

    Add unfinished version of Wanja Chresta's tutorial map.

---
+++ ethernet/maps/tutorial/mission.cs
@@ -0,0 +1,256 @@
+// This script is executed on the server
+
+$MAP_ROOT = "ethernet/maps/tutorial/";
+
+$sgLightEditor::lightDBPath = $MAP_ROOT @ "lights/";
+$sgLightEditor::filterDBPath = $MAP_ROOT @ "filters/";
+sgLoadDataBlocks($sgLightEditor::lightDBPath);
+sgLoadDataBlocks($sgLightEditor::filterDBPath);
+
+//------------------------------------------------------------------------------
+// Material mappings
+//------------------------------------------------------------------------------
+
+%mapping = createMaterialMapping("dark_grey_blue_grid");
+%mapping.sound = $MaterialMapping::Sound::Metal;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("tutorial/dark_blue_grid");
+%mapping.sound = $MaterialMapping::Sound::Metal;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+//%mapping.envmap = "share/textures/tutorial/dark_blue_grid 0.5";
+
+// #############################################################################
+// Scripting stuff now...
+// #############################################################################
+
+package Tutorial {
+    function startNewRound() {
+        /*
+        // Let all players in observer-teams join the red team
+        for( %clientIndex = 0; %clientIndex < ClientGroup.getCount(); %clientIndex++ ) {
+            %client = ClientGroup.getObject( %clientIndex );
+
+            echo(%client);
+            if( %client.team == $Team0 )
+                %client.joinTeam(1);
+        }
+
+
+        for( %clientIndex = 0; %clientIndex < ClientGroup.getCount(); %clientIndex++ ) {
+            %client = ClientGroup.getObject( %clientIndex );
+
+            if( %client.team == $Team1 ) {
+                %client.player.setEnergyLevel(75);
+                %client.togglePlayerForm();
+            }
+        }
+        */
+
+        Parent::startNewRound();
+
+        // create target bots
+        createTargetBotAt(NameToID("genericTargetS"));
+        createTargetBotAt(NameToID("genericTargetM"));
+        createTargetBotAt(NameToID("genericTargetL"));
+        createTargetBotAt(NameToID("sniperTarget"));
+
+    }
+
+    function serverCmdJoinTeam(%client, %team) {
+        Parent::serverCmdJoinTeam(%client, %team);
+
+        if (%team == 0) {
+            displayText(%client, $tutorialText[70]);
+        } else if (%team == 1) {
+        } else if (%team == 2) {
+            displayText(%client, $tutorialText[90]);
+        }
+    }
+};
+activatePackage(Tutorial);
+
+
+// -----------------------------
+// Trigger functions
+// -----------------------------
+
+
+datablock TriggerData(ThrowDiscAtMe) {
+    tickPeriodMS = 200;
+};
+
+function ThrowDiscAtMe::onEnterTrigger(%this, %trigger, %obj) {
+    if (%obj.isCAT) {
+        %origin = NameToID(%trigger.originObjName);
+
+        %obj.client.play2D(DiscIncomingSound);
+        throwDiscAt(%origin, %obj);
+    }
+
+    Parent::onEnterTrigger(%this, %trigger, %obj);
+}
+
+datablock NortDiscData(PussyBlueSeekerDisc : BlueSeekerDisc) {
+    impactDamage         = 20;
+};
+
+function throwDiscAt(%origin, %target) {
+    %disc = new (NortDisc)() {
+        dataBlock         = PussyBlueSeekerDisc;
+        teamId            = 2;
+        initialVelocity  = "0 0 100";
+        initialPosition  = %origin.position;
+        sourceObject      = %origin;
+        sourceSlot       = 0;
+        client            = 0;
+    };
+    MissionCleanup.add(%disc);
+
+    %disc.setTarget(%target);
+    %disc.setTargetingMask($TargetingMask::Disc);
+
+}
+
+
+// -----------------------------
+// Create bots
+// -----------------------------
+
+function createTargetBotAt(%spawnSphere) {
+    if( !isObject($tutBots) ) {
+		$aiPlayers = new Array();
+		MissionCleanup.add($aiPlayers);
+    }
+
+    %player = new AiPlayer() {
+		dataBlock = BlueStandardCat;
+		path = "";
+		teamId = 2;
+	};
+	MissionCleanup.add(%player);
+
+	%pos = getRandomHorizontalPos(%spawnSphere.position,%spawnSphere.radius);
+//	%player.setShapeName("");
+	%player.name = "tutbot_" @ %spawnSphere.getName();
+	%player.setTransform(%pos);
+
+	%player.weapon = 1;
+	%player.charge = 100;
+
+	$tutBots.push_back("",%player);
+
+}
+
+
+// -----------------------------
+// Text display
+// -----------------------------
+//
+datablock DecalData(TextDisplayDecal)
+{
+    sizeX = "3.00";
+    sizeY = "3.00";
+    textureName = "share/textures/eth3/circuit_glow";
+    SelfIlluminated = true;
+    lifeSpan = 600000;
+};
+
+$tutorialTextDecalsCount = 0;
+
+schedule(5000, 0, "showTextDisplayDecals");
+function showTextDisplayDecals() {
+    echo("Redraw Decals");
+    %group = nameToID("TutorialTextTriggers");
+
+    // Delete decals
+    for (%i = 0; %i < $tutorialTextDecalsCount; %i++) {
+        %trig = $tutorialTextDecals[%i];
+        //MissionCleanup.removeObject(%trig); // Is this even necessary?
+        %trig.delete();
+    }
+
+
+    if (%group != -1) {
+        %count = %group.getCount();
+        $tutorialTextDecalsCount = %count;
+
+        if (%count != 0) {
+            for (%i = 0; %i < %count; %i++) {
+                %trig = %group.getObject(%i);
+
+                // Center the decal - this is a dirty hack
+                %x = getWord(%trig.position, 0) + 2.2;
+                %y = getWord(%trig.position, 1) - 2.2;
+                %z = getWord(%trig.position, 2);
+
+                %decal = new sgDecalProjector(%decalPrototype) {
+                    position = %x SPC %y SPC %z;
+                    dataBlock = "TextDisplayDecal";
+                    rotation = "1 0 0 0";
+                    scale = "1 1 1";
+                    canSaveDynamicFields = "1";
+                };
+                $tutorialTextDecals[%i] = %decal;
+                MissionCleanup.add(%decal);
+            }
+
+            schedule(TextDisplayDecal.lifeSpan/3*2, 0, "showTextDisplayDecals");
+        } else 
+            error("showTextDisplayDecals(): could not find any Triggers in the TutorialTextTrigger group.");
+    } else
+        error("showTextDisplayDecals(): could not find the TutorialTextTrigger group.");
+}
+
+//exec($MAP_ROOT @ "tutorial_texts.cs");
+exec("./tutorial_texts.cs");
+
+function displayText(%cl, %message) {
+    %time = 0;
+    %lines = 3;
+    echo("displayText: " @ %message);
+    commandToClient( %cl, 'CenterPrint', %message, %time, %lines );
+}
+
+function undisplayText(%cl) {
+    commandToClient( %cl, 'ClearCenterPrint' );
+}
+
+datablock TriggerData(TextDisplay) {
+    tickPeriodMS = 200;
+};
+
+function TextDisplay::onEnterTrigger(%this, %trigger, %obj) {
+    %text = %trigger.text;
+
+    if (%text >= 70) { // these texts will invoke additional functions
+        if (%text == 75)
+            createTargetBotAt(NameToID("genericTargetS"));
+        if (%text == 76)
+            createTargetBotAt(NameToID("genericTargetM"));
+        if (%text == 77)
+            createTargetBotAt(NameToID("genericTargetL"));
+        if (%text == 78)
+            createTargetBotAt(NameToID("sniperTarget"));
+
+    }
+
+    displayText(%obj.client, $tutorialText[%text]);
+
+    Parent::onEnterTrigger(%this, %trigger, %obj);
+}
+
+function TextDisplay::onLeaveTrigger(%this, %trigger, %obj) {
+    undisplayText(%obj.client);
+
+    Parent::onLeaveTrigger(%this, %trigger, %obj);
+
+}
+
+function TextDisplay::onTickTrigger(%this, %trigger) {
+    Parent::onTickTrigger(%this, %trigger);
+}
+
+
+
+
---
+++ ethernet/maps/tutorial/tutorial_texts.cs
@@ -0,0 +1,50 @@
+$tutorialText[1] = "You are now in the etherform. This is one of the two forms you can be in. Even though this form is a great way to get around and recharge health and energy, you can't interact with the surrounding. To be able to do that, you'll ned to materialise into a CAT. You can only materialise in your own red zones. See that blue light in front of you? That lies inside a red zone. Fly there and press TAB.";
+
+$tutorialText[2] = "After pressing TAB, you'll find yourself in the CAT form. In this tutorial we have prepared multiple locations where you can learn about different aspects of the game. Just move to one of the marked zones, like this one, to see the help to the topic. You're invited to follow the arrows, but of course you don't have to. Move around with WASD.";
+
+$tutorialText[3] = "You've just seen how you transform into CAT form. But you can transform back into etherform as well by pressing TAB.\nSometimes you're even forced back into etherform - it is when you normally would die becasue you took too much damage. Instead of dying, you transform back into etherform. Then you'll have to get back into your own zone to recharge energy and health before going back to become a CAT.";
+
+$tutorialText[4] = "ROTC ethernet is all about zone control. The goal of the game is to catch all of the zones. You do this by walking into them and killing all the enemies. You'll learn more about this in one of the bigger tutorial rooms (the one in your back on the right side).";
+
+// Jump
+$tutorialText[5] = "One jumps with space; but you can even jump in air by pressing space again to get even higher! Try to get to the upper platform!";
+$tutorialText[6] = "Congratulatons, you made it to the top! Keep in mind though, that jumping in air uses energy. Energy is your most important resource, so only use it if you really have to!";
+
+$tutorialText[7] = "Energy is your most important stat besides your health. Energy is used for almost everything: Power for your weapons, shield, energy for jumps. Mastering the household of your energy is one of the most important and more difficult parts of rotc. If you fire too much on your enemy, you'll leave yourself vurnerable because your shield is down.";
+
+
+// Run
+$tutorialText[10] = "We also have something like running: Use the board by pressing CTRL to slide through the map. You only have a finite time to use it; afterwards you'll have to wait for it to recharge.";
+$tutorialText[11] = "The board is an awesome tool to get around fast - and it doesn't use any energy, which is perfect to get behind enemy lines fast and shoot them in their backs, or go even further and taking the next but one zone; while your buddies deal with the rest of the enemies.";
+
+$tutorialText[15] = "This is the weapons room. We have a lot of different weapons you'll learn about in a second. The most difficult but also most enjoyable is your seeker and deflecter disc. You'll learn about it between the huge triangles. In front of you is the part about the standard weapons like MGs and grenades. On your left, on the far end of the room is the part about the sniper ROFL. You'll enjoy that! Afterwards, come back here and go into another room.";
+
+// weapons
+$tutorialText[20] = "The weapons. There are five different weapons in rotc. You always have the trident, a main gun, grenades and discs. Move to the weapons in front of you to learn more about them.";
+
+$tutorialText[21] = "Every CAT carries a Trident. The Trident is a powerful but tricky weapon. It always fires 9 shots at once. Because of this, it uses a lot of energy and has a huge recoil, but is able to deal up to 90 points of damage at once.";
+$tutorialText[22] = "The MG fires tiny grenades that explode near enemies or on impact. While a single grenade is pretty weak, the fact that you don't have to be 100% accurate to deal damage to enemies combined with a relatively high rate of fire turn the MG into a formidable weapon. Like the Sniper ROFL you can choose it while you are in etherform by pressing 1.";
+$tutorialText[23] = "A grenade will implode shortly after it has been thrown. To throw a grenade you need to hold down and then release the middle mouse button. The grenade is thrown farther the longer the mouse button was held down. There's also the possibility to throw a Grenade instantly at full force by holding down shift and clicking the middle mouse button. Grenades don't consume energy. Instead, they draw from their own separate ammo supply. The grenade ammo supply regenerates slowly by itself, but the regeneration can be sped up by dealing damage to enemies.";
+$tutorialText[24] = "A seeker Disc will track its target until it has hit it, missed it, was deflected by another Disc, has bounced a few times or has expired. While a disc does not deal a lot of damage compared to its rate of fire, it uses absolutely no energy and will return 20 points of energy to the CAT that launched it when it hits its target. To throw a disc, you first need to get a target-lock on an enemy CAT. The seeker is then thrown by a quick click of the right mouse button. This is the hardest weapon to master in all of ROTC. We have a own section in this tutorial to teach you how it works.";
+
+// sniper rofl
+$tutorialText[25] = "Sniper ROFL. To switch to the Sniper ROFL press tab to dematerialize. When you're in etherform, press 2 to choose the sniper ROFL as your primary, and materialize again by pressing tab.";
+
+$tutorialText[26] = "The Sniper ROFL is... kind of a sniper rifle. The sniper rofl needs to be charged before it can fire. While charging doesn't take long and doesn't consume energy, you can't run fast or use the Etherboard while doing it. Hold down the left mouse button to charge, release it to fire. We've created a target for you over there - try to kill it; it won't shoot back ;).";
+
+// disc
+$tutorialText[30] = "This part teaches you about defending against seeker discs. When an enemy throws a disc at you, you'll get an acustic signal (beep beep). To defend against the incoming disc, look at it - it will have a white triangle marking it. If you center it in your crosshair, the disc will be locked - indicated by a white dot. In that instance, press the right mouse button. This will release an interceptor disc to deflect the attacking disc. Try it! If you step forward, you'll have a disc thrown at you.";
+
+///// all texts >70 have a special meaning and will invoke additional functions - not only display text
+
+$tutorialText[70] = "Hello, welcome to the rotc tutorial. Please join the red team by pressing ESC on your keyboard - choosing \"Join Team\" and then clicking on the proper button.";
+
+$tutorialText[75] = "Short target created";
+$tutorialText[76] = "Mid target created";
+$tutorialText[77] = "Far target created";
+$tutorialText[78] = "Sniper target created";
+
+// error msgs
+
+$tutorialText[90] = "No! This is the blue team - you want to join the red team.";
+
