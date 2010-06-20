d77e571eb7bbba97e6e234ea13bbb8694ef4c096
Michael Goldener <mg@wasted.ch>
2010-06-20 22:41:46 +0200

    Work on sp_tutorial map.

--- ethernet/maps/sp_tutorial/stages.cs
+++ ethernet/maps/sp_tutorial/stages.cs
@@ -1,13 +1,14 @@
 // Stages:
-// 1 = Observe
+// 1 = Observer
 // 2 = Wrong team
 // 3 = Initial etherform
 // 4 = Initial CAT
 // 5 = Captured first zone
 // 6 = Captured long trench zone
 // 7 = jump beams
-// 8 = jumped beams
-// 9 = jumped pit
+// 8 = jumo wall of beams
+// 9 = jump pit
+// 10 = jumped pit
 
 
 datablock TriggerData(Map_StageTrigger) {
@@ -15,48 +16,54 @@ datablock TriggerData(Map_StageTrigger) {
 };
 
 function Map_StageTrigger::onEnterTrigger(%this, %trigger, %obj) {
+    Parent::onEnterTrigger(%this, %trigger, %obj);
+
+	if(%obj.infoPoint)
+		return;
+
+	if(!%obj.isCAT)
+		return;
+
 	if(%trigger.getName() $= "StageTrigger2" && $Map::Stage == 4)
 	{
 		$Map::Stage = 5;
-		map_displayCenter($Map::Text::3);
+		map_activateInfoPoint(2);
+		map_displayObjective($Map::Text::SeeInfoBot);
 	}
-	else if(%trigger.getName() $= "StageTrigger3" && $Map::Stage == 5)
+	else if(%trigger.getName() $= "StageTrigger3" && $Map::Stage < 6)
 	{
 		$Map::Stage = 6;
-		map_displayCenter($Map::Text::4);
+		map_activateInfoPoint(3);
+		map_displayObjective($Map::Text::SeeInfoBot);
 	}
-	else if(%trigger.getName() $= "StageTrigger4" && $Map::Stage == 6)
+	else if(%trigger.getName() $= "StageTrigger4" && $Map::Stage < 7)
 	{
 		$Map::Stage = 7;
-		map_displayCenter($Map::Text::5);
+		map_activateInfoPoint(4);
+		map_displayObjective($Map::Text::SeeInfoBot);
 	}
-	else if(%trigger.getName() $= "StageTrigger5" && $Map::Stage == 7)
+	else if(%trigger.getName() $= "StageTrigger5" && $Map::Stage < 8)
 	{
 		$Map::Stage = 8;
-		map_displayCenter($Map::Text::6);
+		map_activateInfoPoint(5);
+		map_displayObjective($Map::Text::SeeInfoBot);
 	}
-	else if(%trigger.getName() $= "StageTrigger6" && $Map::Stage == 8)
-	{
-		map_displayCenter($Map::Text::7);
-	}
-	else if(%trigger.getName() $= "StageTrigger7" && $Map::Stage == 8)
+	else if(%trigger.getName() $= "StageTrigger6" && $Map::Stage < 9)
 	{
 		$Map::Stage = 9;
-		map_displayCenter($Map::Text::8);
+		map_activateInfoPoint(6);
+		map_displayObjective($Map::Text::SeeInfoBot);
 	}
-    Parent::onEnterTrigger(%this, %trigger, %obj);
+	else if(%trigger.getName() $= "StageTrigger7" && $Map::Stage < 10)
+	{
+		$Map::Stage = 10;
+		map_activateInfoPoint(7);
+		map_displayObjective($Map::Text::SeeInfoBot);
+	}
+
 }
 
 function Map_StageTrigger::onLeaveTrigger(%this, %trigger, %obj) {
-	if(%trigger.getName() $= "StageTrigger3" && $Map::Stage == 6)
-	{
-		map_clearCenter();
-	}
-	else if(%trigger.getName() $= "StageTrigger6" && $Map::Stage == 8)
-	{
-		map_clearCenter();
-	}
-
     Parent::onLeaveTrigger(%this, %trigger, %obj);
 }
 
