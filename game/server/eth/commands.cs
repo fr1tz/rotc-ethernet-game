95f53dfa6c37c644b75d4253bf2b1e8fb1b7ff4b
Michael Goldener <mg@wasted.ch>
2010-11-25 14:08:15 +0100

    Improve in-game menu: Add "ROTC: Ethernet in a nutshell" page.

--- game/server/eth/commands.cs
+++ game/server/eth/commands.cs
@@ -12,34 +12,41 @@ function serverCmdHowToPlay(%client, %page)
 		%page = 1;
 
 	%newtxt = %newtxt @ 
+		"<font:Arial:16>" @
 		"<< <a:cmd MainMenu>Main Menu</a>\n\n" @
-		"<spush><font:Arial:24>How to play ROTC: Ethernet?<spop>\n\n";
+		"<just:center><spush><font:Arial:20>How to play ROTC: Ethernet?<spop>\n\n";
 
 	switch(%page)
 	{
 		case 1:
-			%title = "1. Basics";
+			%title = "1. ROTC: Ethernet in a nutshell";
 			%prev = "";
-			%next = "<a:cmd HowToPlay 2>Life as Etherform</a>";
-			%fileName = "game/server/eth/help/basics.rml";
+			%next = "<a:cmd HowToPlay 2>Basics</a>";
+			%fileName = "game/server/eth/help/summary.rml";
 
 		case 2:
-			%title = "Playing as Etherform";
-			%prev = "<a:cmd HowToPlay 1>Basics</a>";
-			%next = "<a:cmd HowToPlay 3>Playing in CAT form</a>";
+			%title = "2. Basics";
+			%prev = "<a:cmd HowToPlay 1>ROTC: Ethernet in a nutshell</a>";
+			%next = "<a:cmd HowToPlay 3>Playing as Etherform</a>";
+			%fileName = "game/server/eth/help/basics.rml";
+
+		case 3:
+			%title = "3. Playing as Etherform";
+			%prev = "<a:cmd HowToPlay 2>Basics</a>";
+			%next = "<a:cmd HowToPlay 4>Playing in CAT form</a>";
 			%fileName = "game/server/eth/help/etherform.rml";
 			break;
 
-		case 3:
-			%title = "3. Playing in CAT form";
-			%prev = "<a:cmd HowToPlay 2>Playing as Etherform</a>";
-			%next = "<a:cmd HowToPlay 4>Weapons</a>";
+		case 4:
+			%title = "4. Playing in CAT form";
+			%prev = "<a:cmd HowToPlay 3>Playing as Etherform</a>";
+			%next = "<a:cmd HowToPlay 5>Weapons</a>";
 			%fileName = "game/server/eth/help/cat.rml";
 			break;
 
-		case 4:
-			%title = "4. Weapons";
-			%prev = "<a:cmd HowToPlay 3>Playing in CAT form</a>";
+		case 5:
+			%title = "5. Weapons";
+			%prev = "<a:cmd HowToPlay 4>Playing in CAT form</a>";
 			%next = "";
 			%fileName = "game/server/eth/help/weapons.rml";
 			break;
@@ -47,19 +54,21 @@ function serverCmdHowToPlay(%client, %page)
 		default:
 			%title = "0. Index";
 			%prev = "";
-			%next = "<a:cmd HowToPlay 1>Basics</a>";
+			%next = "<a:cmd HowToPlay 1>ROTC: Ethernet in a nutshell</a>";
 			%fileName = "game/server/eth/help/index.rml";
 			break;
 	}
 
-	%nav = "";
+	%nav = "<spush><font:Arial:13>";
 	if(%prev !$= "")
 		%nav = %nav @ "Prev:" SPC %prev SPC "|";
 	%nav = %nav SPC "<a:cmd HowToPlay 0>Index</a>";
 	if(%next !$= "")
 		%nav = %nav SPC "| Next:" SPC %next;
+	%nav = %nav @ "<spop>";
 
-	%newtxt = %newtxt @ "<just:center><spush><font:Arial:22>" @ %title @ "<spop>\n" @ %nav @ "<just:left>\n\n";
+	%newtxt = %newtxt @ %nav @ "\n\n" @
+		"<spush><font:Arial:24>" @ %title @ "<spop><just:left>\n\n";
 
 	%file = new FileObject();
 	%file.openForRead(%fileName);
