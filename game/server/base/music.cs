ca90a2a70e35888135957f12fda99002e489915a
Michael Goldener <mg@wasted.ch>
2010-11-27 11:16:23 +0100

    Play victory/defeat music depending on player's team.

--- game/server/base/music.cs
+++ game/server/base/music.cs
@@ -22,6 +22,17 @@ function playMusicAll(%profile, %immediately)
 	}
 }
 
+function playMusic(%teamId, %profile, %immediately)
+{
+	%count = ClientGroup.getCount();
+	for(%i = 0; %i < %count; %i++)
+	{
+		%obj = ClientGroup.getObject(%i);
+		if(%obj.team.teamId == %teamId)
+			%obj.playMusic(%profile, %immediately);
+	}
+}
+
 datablock AudioProfile(MusicStartup)
 {
 	filename = "share/music/eth1/work1_ethernet_startup.wav";
@@ -36,42 +47,42 @@ datablock AudioProfile(MusicIdle)
 	preload = true;
 };
 
-datablock AudioProfile(MusicRedVictory1)
+datablock AudioProfile(MusicVictory1)
 {
 	filename = "share/music/eth1/work1_ethernet_victory1.wav";
 	description = AudioMusic;
 	preload = true;
 };
 
-datablock AudioProfile(MusicRedVictory2)
+datablock AudioProfile(MusicVictory2)
 {
 	filename = "share/music/eth1/work1_ethernet_victory2.wav";
 	description = AudioMusic;
 	preload = true;
 };
 
-datablock AudioProfile(MusicRedVictory3)
+datablock AudioProfile(MusicVictory3)
 {
 	filename = "share/music/eth1/work1_ethernet_victory3.wav";
 	description = AudioMusic;
 	preload = true;
 };
 
-datablock AudioProfile(MusicBlueVictory1)
+datablock AudioProfile(MusicDefeat1)
 {
 	filename = "share/music/eth1/work1_ethernet_defeat1.wav";
 	description = AudioMusic;
 	preload = true;
 };
 
-datablock AudioProfile(MusicBlueVictory2)
+datablock AudioProfile(MusicDefeat2)
 {
 	filename = "share/music/eth1/work1_ethernet_defeat2.wav";
 	description = AudioMusic;
 	preload = true;
 };
 
-datablock AudioProfile(MusicBlueVictory3)
+datablock AudioProfile(MusicDefeat3)
 {
 	filename = "share/music/eth1/work1_ethernet_defeat3.wav";
 	description = AudioMusic;
@@ -131,15 +142,33 @@ function serverUpdateMusic()
 			%immediately = true;
 
 		if(%gameBalance >= 0.8)
-			playMusicAll(MusicRedVictory3, %immediately);
+		{
+			playMusic(0, MusicVictory3, %immediately);
+			playMusic(1, MusicVictory3, %immediately);
+			playMusic(2, MusicDefeat3, %immediately);
+		}
 		else if(%gameBalance >= 0.4)
-			playMusicAll(MusicRedVictory2, %immediately);
+		{
+			playMusic(0, MusicVictory2, %immediately);
+			playMusic(1, MusicVictory2, %immediately);
+			playMusic(2, MusicDefeat2, %immediately);
+		}
 		else if(%gameBalance <= -0.8)
-			playMusicAll(MusicBlueVictory3, %immediately);
+		{
+			playMusic(0, MusicVictory3, %immediately);
+			playMusic(1, MusicDefeat3, %immediately);
+			playMusic(2, MusicVictory3, %immediately);
+		}
 		else if(%gameBalance <= -0.4)
-			playMusicAll(MusicBlueVictory2, %immediately);
+		{
+			playMusic(0, MusicVictory2, %immediately);
+			playMusic(1, MusicDefeat2, %immediately);
+			playMusic(2, MusicVictory2, %immediately);
+		}
 		else
-			playMusicAll(MusicRedVictory1, %immediately);
+		{
+			playMusicAll(MusicVictory1, %immediately);
+		}
 
 		$Music::Action = true;
 	}
