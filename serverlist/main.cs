7fe8322d2aeb596fc31f8e77f96707455cf77498
Michael Goldener <mg@wasted.ch>
2010-11-29 18:12:01 +0100

    Update serverlist mod.

--- serverlist/main.cs
+++ serverlist/main.cs
@@ -13,31 +13,48 @@ function printServers()
 	for (%i = 0; %i < %sc; %i++) {
 		setServerInfo(%i);
         %prefix = "#server" SPC $ServerInfo::Address;
-		echo(%prefix SPC "game_version" SPC $ServerInfo::GameVersion);
-		echo(%prefix SPC "name" SPC $ServerInfo::Name);
+		echo(%prefix SPC "arena" SPC $ServerInfo::MissionType SPC
+			$ServerInfo::MissionName);
 		echo(%prefix SPC "ping" SPC $ServerInfo::Ping);
-		echo(%prefix SPC "mod" SPC $ServerInfo::ModString);
 		echo(%prefix SPC "player_count" SPC $ServerInfo::PlayerCount @ " of " @ $ServerInfo::MaxPlayers);
-		echo(%prefix SPC "map_name" SPC $ServerInfo::MissionName);
-		echo(%prefix SPC "map_homepage" SPC $ServerInfo::MissionHomepage);
-		echo(%prefix SPC "info" SPC $ServerInfo::Info);
+		echo(%prefix SPC "hoster" SPC $ServerInfo::Name);
+		echo(%prefix SPC "description" SPC $ServerInfo::Info);
 	}
 }
 
 function queryServers()
 {
-	queryMasterServer(
-		0,			 // Query flags
-		$Client::GameTypeQuery,		 // gameTypes
-		$Client::MissionTypeQuery,	 // missionType
-		0,			 // minPlayers
-		100,		  // maxPlayers
-		0,			 // maxBots
-		2,			 // regionMask
-		0,			 // maxPing
-		100,		  // minCPU
-		0			  // filterFlags
-	);
+	if($lan)
+	{
+		queryLANServers(
+			28000,		// lanPort for local queries
+			0,			 // Query flags
+			$Client::GameTypeQuery,		 // gameTypes
+			$Client::MissionTypeQuery,	 // missionType
+			0,			 // minPlayers
+			100,		  // maxPlayers
+			0,			 // maxBots
+			2,			 // regionMask
+			0,			 // maxPing
+			100,		  // minCPU
+			0			  // filterFlags
+		);
+	}
+	else
+	{
+		queryMasterServer(
+			0,			 // Query flags
+			$Client::GameTypeQuery,		 // gameTypes
+			$Client::MissionTypeQuery,	 // missionType
+			0,			 // minPlayers
+			100,		  // maxPlayers
+			0,			 // maxBots
+			2,			 // regionMask
+			0,			 // maxPing
+			100,		  // minCPU
+			0			  // filterFlags
+		);
+	}
 }
 
 function onServerQueryStatus(%status, %msg, %value)
@@ -50,6 +67,34 @@ function onServerQueryStatus(%status, %msg, %value)
 }
 
 package ServerList {
+
+function displayHelp() {
+	Parent::displayHelp();
+	error(
+		"Serverlist Mod options:\n"@
+		"  -lan                Query lan servers\n"
+	);
+}
+
+function parseArgs()
+{
+	Parent::parseArgs();
+
+	$lan = false;
+
+	for (%i = 1; %i < $Game::argc ; %i++)
+	{
+		%arg = $Game::argv[%i];
+		%nextArg = $Game::argv[%i+1];
+		%hasNextArg = $Game::argc - %i > 1;
+	
+		switch$ (%arg)
+		{
+			case "-lan":
+				$lan = true;
+		}
+	}
+}
 
 function onStart()
 {
