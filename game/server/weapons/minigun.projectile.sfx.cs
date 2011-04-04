a5cfc060d903486786102726db324ae8b9b2d82e
Michael Goldener <mg@wasted.ch>
2011-04-04 14:07:04 +0200

    Use different sounds for minigun's missed enemy effect.

--- game/server/weapons/minigun.projectile.sfx.cs
+++ game/server/weapons/minigun.projectile.sfx.cs
@@ -19,7 +19,13 @@ datablock AudioProfile(MinigunProjectileFlybySound)
 
 datablock AudioProfile(MinigunProjectileMissedEnemySound)
 {
-	filename = "share/sounds/rotc/flyby1.wav";
+	filename = "share/sounds/rotc/ricochet1-1.wav";
+	alternate[0] = "share/sounds/rotc/ricochet1-1.wav";
+	alternate[1] = "share/sounds/rotc/ricochet1-2.wav";
+	alternate[2] = "share/sounds/rotc/ricochet1-3.wav";
+	alternate[3] = "share/sounds/rotc/ricochet1-4.wav";
+	alternate[4] = "share/sounds/rotc/ricochet1-5.wav";
+	alternate[5] = "share/sounds/rotc/ricochet1-6.wav";	
 	description = AudioClose3D;
 	preload = true;
 };
