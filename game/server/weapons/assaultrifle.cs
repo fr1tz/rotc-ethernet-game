2121baa97abb1198a609a935754e081169010083
Michael Goldener <mg@wasted.ch>
2011-04-08 11:15:44 +0200

    Convert assault rifle to use the conventional Projectile class.

--- game/server/weapons/assaultrifle.cs
+++ game/server/weapons/assaultrifle.cs
@@ -33,7 +33,7 @@ datablock ShapeBaseImageData(HolsteredAssaultRifleImage)
 //-----------------------------------------------------------------------------
 // projectile datablock...
 
-datablock TracerProjectileData(RedAssaultRifleProjectile1)
+datablock ProjectileData(RedAssaultRifleProjectile1)
 {
 	stat = "br";
 
@@ -93,7 +93,7 @@ datablock TracerProjectileData(RedAssaultRifleProjectile1)
 	lightColor  = "1.0 0.8 0.2";
 };
 
-datablock TracerProjectileData(RedAssaultRifleProjectile2 : RedAssaultRifleProjectile1)
+datablock ProjectileData(RedAssaultRifleProjectile2 : RedAssaultRifleProjectile1)
 {
 	posOffset = "0 0 0.1";
 	velOffset = "0 0.025";
@@ -118,7 +118,7 @@ function RedAssaultRifleProjectile2::onCollision(%this,%obj,%col,%fade,%pos,%nor
 
 //--------------------------------------------------------------------------
 
-datablock TracerProjectileData(BlueAssaultRifleProjectile1 : RedAssaultRifleProjectile1)
+datablock ProjectileData(BlueAssaultRifleProjectile1 : RedAssaultRifleProjectile1)
 {
 	projectileShapeName = "share/shapes/rotc/weapons/assaultrifle/projectile2.cyan.dts";    
 	explosion = CyanAssaultRifleProjectileImpact;
@@ -130,7 +130,7 @@ datablock TracerProjectileData(BlueAssaultRifleProjectile1 : RedAssaultRifleProj
 	lightColor  = "0.0 0.8 1.0";
 };
 
-datablock TracerProjectileData(BlueAssaultRifleProjectile2 : RedAssaultRifleProjectile2)
+datablock ProjectileData(BlueAssaultRifleProjectile2 : RedAssaultRifleProjectile2)
 {
 	projectileShapeName = "share/shapes/rotc/weapons/assaultrifle/projectile2.cyan.dts";    
 	explosion = CyanAssaultRifleProjectileImpact;
@@ -183,8 +183,8 @@ datablock ShapeBaseImageData(RedAssaultRifleImage)
 
 	// light properties...
 	lightType = "WeaponFireLight";
-	lightColor = "0 1 0";
-	lightTime = 1000;
+	lightColor = "1 0.5 0";
+	lightTime = 100;
 	lightRadius = 8;
 	lightCastsShadows = false;
 	lightAffectsShapes = true;
@@ -220,11 +220,11 @@ datablock ShapeBaseImageData(RedAssaultRifleImage)
 		stateSequence[2]                 = "idle";
 		
 		stateName[3]                     = "Fire";
-		stateTransitionOnTimeout[3]      = "Fire2";
-		stateTimeoutValue[3]             = 0.0;
-		stateFire[8]                     = true;
-		stateFireProjectile[3]           = RedAssaultRifleProjectile1;
-		stateRecoil[3]                   = MediumRecoil;
+		stateTransitionOnTriggerUp[3]    = "KeepAiming";
+		//stateTimeoutValue[3]             = 0.0;
+		stateFire[3]                     = true;
+		//stateFireProjectile[3]           = RedAssaultRifleProjectile1;
+		//stateRecoil[3]                   = MediumRecoil;
 		stateAllowImageChange[3]         = false;
 		stateEjectShell[3]               = true;
 		stateArmThread[3]                = "aimrifle";
@@ -266,13 +266,66 @@ datablock ShapeBaseImageData(RedAssaultRifleImage)
 	//-------------------------------------------------
 };
 
+function RedAssaultRifleImage::onFire(%this, %obj, %slot)
+{
+	%projectile = %this.projectile;
+
+	%objectVelocity = %obj.getVelocity();
+	%muzzlePoint = %obj.getMuzzlePoint(%slot);
+	%muzzleVector = %obj.getMuzzleVector(%slot);	
+	%muzzleTransform = createOrientFromDir(%muzzleVector);
+	
+	%pos[0] = "0 0 0";
+	%vec[0] = "0 1 0.005";
+	%pos[1] = "0 0 0.1";
+	%vec[1] = "0 1 0.025";
+	
+	for(%i = 0; %i < 2; %i++)
+	{
+      %position =	VectorAdd(
+			%muzzlePoint, 
+			MatrixMulVector(%muzzleTransform, %pos[%i])
+		);		
+		%velocity = VectorAdd(
+			VectorScale(MatrixMulVector(%muzzleTransform, %vec[%i]), %projectile.muzzleVelocity),
+			VectorScale(%objectVelocity, %projectile.velInheritFactor)
+		);	      
+
+		// create the projectile object...
+		%p = new Projectile() {
+			dataBlock       = %projectile;
+			teamId          = %obj.teamId;
+			initialVelocity = %velocity;
+			initialPosition = %position;
+			sourceObject    = %obj;
+			sourceSlot      = %slot;
+			client	    = %obj.client;
+		};
+		MissionCleanup.add(%p);
+
+		%obj.setEnergyLevel(%obj.getEnergyLevel() - %projectile.energyDrain);
+	 
+		%target = %obj.getImageTarget(%slot);
+		if(isObject(%target))
+			%p.setTarget(%target);	
+	}	
+        
+	return %p;
+}
+
 //------------------------------------------------------------------------------
 
 datablock ShapeBaseImageData(BlueAssaultRifleImage : RedAssaultRifleImage)
 {
 	shapeFile = "share/shapes/rotc/weapons/assaultrifle/image3.cyan.dts";
 	projectile = BlueAssaultRifleProjectile1;
-	stateFireProjectile[3] = BlueAssaultRifleProjectile1;
-	stateFireProjectile[8] = BlueAssaultRifleProjectile2;
+	lightColor = "0 0.5 1";
+	//stateFireProjectile[3] = BlueAssaultRifleProjectile1;
+	//stateFireProjectile[8] = BlueAssaultRifleProjectile2;
 };
+
+function BlueAssaultRifleImage::onFire(%this, %obj, %slot)
+{
+	RedAssaultRifleImage::onFire(%this, %obj, %slot);
+}
 
