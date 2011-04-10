762252e4d9d9bd21facf606bd4f031633d9730d0
Michael Goldener <mg@wasted.ch>
2011-04-10 16:03:22 +0200

    Bring back the old nort1 map as /env/nat/parc.

---
+++ game/arenas/rotc-ethernet/nat-parc/mission.env.cs
@@ -0,0 +1,35 @@
+// This script is executed on the server
+
+$ENV_ROOT = "game/arenas/rotc-ethernet/nat-parc/";
+
+//------------------------------------------------------------------------------
+// Lights
+//------------------------------------------------------------------------------
+
+$sgLightEditor::lightDBPath = $ENV_ROOT @ "lights/";
+$sgLightEditor::filterDBPath = $ENV_ROOT @ "filters/";
+sgLoadDataBlocks($sgLightEditor::lightDBPath);
+sgLoadDataBlocks($sgLightEditor::filterDBPath);
+
+//------------------------------------------------------------------------------
+// Material mappings
+//------------------------------------------------------------------------------
+
+%mapping = createMaterialMapping("gray3");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("grass2");
+%mapping.sound = $MaterialMapping::Sound::Soft;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("rock");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+%mapping = createMaterialMapping("stone");
+%mapping.sound = $MaterialMapping::Sound::Hard;
+%mapping.color = "0.3 0.3 0.3 0.4 0.0";
+
+
+
