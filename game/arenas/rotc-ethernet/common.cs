87091a1ee1f26e12a7a987891e5c4fe7df7109ee
Michael Goldener <mg@wasted.ch>
2011-12-04 10:44:28 +0100

    Update ROTC: Ethernet .mis files to use a common.cs file.

---
+++ game/arenas/rotc-ethernet/common.cs
@@ -0,0 +1,3 @@
+$MissionInfo::Type = "ROTC: Ethernet" SPC $GameVersionString;
+$MissionInfo::TypeDesc = "Capture all the opposing team's zones to win.";
+$MissionInfo::InitScript = "game/server/missions/rotc-eth.cs";
