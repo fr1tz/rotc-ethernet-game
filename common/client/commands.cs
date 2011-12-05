4a8574be7a3953d61027a6885cd5154d6b7c7b5e
Michael Goldener <mg@wasted.ch>
2011-12-05 05:11:09 +0100

    Try to fix possible way for server to exploit client.

--- common/client/commands.cs
+++ common/client/commands.cs
@@ -12,6 +12,20 @@ function clientCmdCookie(%name, %value)
 	echo("Name:" SPC %name);
 	echo("Value:" SPC %value);
 
+   // Make sure the server can't interject code into our prefs.cs file!!!
+   %legal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+   %len = strlen(%name);
+   for(%i = 0; %i < %len; %i++)
+   {
+      %c = getSubStr(%name, %i, 1);
+
+      if(strpos(%legal, %c) < 0)
+      {
+         error("Illegal cookie name! Contains" SPC %c);
+         return;
+      }
+   }
+
 	$Pref::Cookie_[%name] = %value;
 }
 
