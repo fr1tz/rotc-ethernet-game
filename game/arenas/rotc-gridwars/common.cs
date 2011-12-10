b1effa595ce21375effeaceda7f6b79f2e692e5b
Michael Goldener <mg@wasted.ch>
2011-12-10 11:09:45 +0100

    Add GridWars arena files.

---
+++ game/arenas/rotc-gridwars/common.cs
@@ -0,0 +1,3 @@
+$MissionInfo::Type = "ROTC: Grid Wars" SPC "(" @ $GameVersionString @ ")";
+$MissionInfo::TypeDesc = "Build and power grid beacons to take over territory.";
+$MissionInfo::InitScript = "game/server/missions/rotc-gridwars.cs";
