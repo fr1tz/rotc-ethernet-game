4b8a9a77ca6bee3107fb7c74cb1dc0b1d28d6115
Michael Goldener <mg@wasted.ch>
2011-12-10 11:09:06 +0100

    Add B/CONS arena files.

---
+++ game/arenas/rotc-bcons/common.cs
@@ -0,0 +1,3 @@
+$MissionInfo::Type = "ROTC: B/CONS (p.1)";
+$MissionInfo::TypeDesc = "Build and power grid beacons to take over territory.";
+$MissionInfo::InitScript = "game/server/missions/rotc-bcons.cs";
