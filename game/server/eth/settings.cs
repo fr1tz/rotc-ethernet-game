2f0f85f5560677668d2f56f89cfc754986a89ef3
Michael Goldener <mg@wasted.ch>
2011-12-15 08:34:35 +0100

    Tweak "Game Settings" menu.

--- game/server/eth/settings.cs
+++ game/server/eth/settings.cs
@@ -8,95 +8,81 @@ function showSettings(%client, %section)
 	%newtxt = om_init();
 	%client.beginMenuText();
 	
-	%note = "";
+	%newtxt = %newtxt @ om_head(%client, $Game::GameTypeString SPC "Settings");	
 
-	if(%section == 1)
-	{
-		%newtxt = %newtxt @ om_head(%client, "Settings", "ShowSettings");	
-		
-		%newtxt = %newtxt @ %note;
+	// Handicap
+	%hand = (%client.handicap >= 0)?%client.handicap:1;
+	%newtxt = %newtxt @ "<lmargin:0>Handicap:\n<lmargin:25>";
+	%newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ ((%hand - 0.1 >= 0)?%hand - 0.1:0) @ ">\<</a>";
+	for (%i = 0; %i <= 10; %i += 1) {
+		if (mfloor(100*mabs(%i/10 - %hand)) == 0) {
+			 %newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ %i/10 @ ">#</a>";
+		} else {
+			 %newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ %i/10 @ ">-</a>";
+		}
 	}
-	else
+	%newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ ((%hand + 0.1 <= 1)?%hand + 0.1:1) @ ">\></a>";
+	%s = %hand;
+	if(%s == 0) %s = "0.0";
+	else if(%s == 1) %s = "1.0";				
+	%newtxt = %newtxt SPC %s SPC "[<a:cmd HowToPlay H>what's this?</a>]\n";
+	if (%hand == 0)
 	{
-		%newtxt = %newtxt @ om_head(%client, "Settings");
-		
-		%newtxt = %newtxt @ %note @ "\n\n";
-
-		// Handicap
-		%hand = (%client.handicap >= 0)?%client.handicap:1;
-		%newtxt = %newtxt @ "<lmargin:0>Handicap:\n<lmargin:25>";
-		%newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ ((%hand - 0.1 >= 0)?%hand - 0.1:0) @ ">\<</a>";
-		for (%i = 0; %i <= 10; %i += 1) {
-			if (mfloor(100*mabs(%i/10 - %hand)) == 0) {
-				 %newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ %i/10 @ ">#</a>";
-			} else {
-				 %newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ %i/10 @ ">-</a>";
-			}
-		}
-		%newtxt = %newtxt @ "<a:cmd SetSetting handicap/" @ ((%hand + 0.1 <= 1)?%hand + 0.1:1) @ ">\></a>";
-		%s = %hand;
-		if(%s == 0) %s = "0.0";
-		else if(%s == 1) %s = "1.0";				
-		%newtxt = %newtxt SPC %s SPC "[<a:cmd HowToPlay H>what's this?</a>]\n";
-		if (%hand == 0)
-		{
-			%newtxt = %newtxt @ "<spush><color:ff4444>Your handicap is 0.0, this means you'll do no damage to players with a handicap of 1.0.<spop>\n";
-		}		
-		%newtxt = %newtxt @ "\n";			
-		
-		//Initial display
-
-		%newtxt = %newtxt @ "<lmargin:0>Initially display:\n<lmargin:25>";	
-		if(%client.initialTopHudMenu !$= "newbiehelp")
-			%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/0>";		
-		%newtxt = %newtxt @ "Newbie helper";	
-		if(%client.initialTopHudMenu !$= "newbiehelp")
-			%newtxt = %newtxt @ "</a>";			
-		%newtxt = %newtxt @ " | ";		
-		if(%client.initialTopHudMenu !$= "healthbalance")
-			%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/1>";		
-		%newtxt = %newtxt @ "Health balance";	
-		if(%client.initialTopHudMenu !$= "healthbalance")
-			%newtxt = %newtxt @ "</a>";			
-		%newtxt = %newtxt @ " | ";				
-		if(%client.initialTopHudMenu !$= "nothing")
-			%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/2>";		
-		%newtxt = %newtxt @ "Nothing";	
-		if(%client.initialTopHudMenu !$= "nothing")
-			%newtxt = %newtxt @ "</a>";			
-		%newtxt = %newtxt @ "\n\n";				
-		
-		%n = 0;
-		%schemeName[%n] = "";
-		%schemeDesc[%n] = "Based on team"; %n++;
-		%schemeName[%n] = "fr1tz";
-		%schemeDesc[%n] = "fr1tz"; %n++;		
-		%schemeName[%n] = "kurrata";
-		%schemeDesc[%n] = "kurrata"; %n++;		
-		%schemeName[%n] = "c&c";
-		%schemeDesc[%n] = "c&c"; %n++;			
-		%schemeName[%n] = "cga1dark";
-		%schemeDesc[%n] = "CGA #1 Dark"; %n++;		
-		%schemeName[%n] = "cga1light";
-		%schemeDesc[%n] = "CGA #1 Light"; %n++;	
-		
-		%newtxt = %newtxt @ "<lmargin:0>HUD Colors:\n<lmargin:25>";			
-		for(%i = 0; %i < %n; %i++)
-		{
-			if(%client.hudColor !$= %schemeName[%i])
-				%newtxt = %newtxt @ "<a:cmd SetSetting hudColor/" @ %schemeName[%i] @ ">";
-			
-			%newtxt = %newtxt @ %schemeDesc[%i];					
-				
-			if(%client.hudColor !$= %schemeName[%i])
-				%newtxt = %newtxt @ "</a>";				
-
-			%newtxt = %newtxt @ "   ";								
-		}
-		%newtxt = %newtxt @ "\n\n";					
+		%newtxt = %newtxt @ "<spush><color:ff4444>Your handicap is 0.0, this means you'll do no damage to players with a handicap of 1.0.<spop>\n";
+	}		
+	%newtxt = %newtxt @ "\n";			
 	
-	}
+	//Initial display
 
+	%newtxt = %newtxt @ "<lmargin:0>Initially display:\n<lmargin:25>";	
+	if(%client.initialTopHudMenu !$= "newbiehelp")
+		%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/0>";		
+	%newtxt = %newtxt @ "Newbie helper";	
+	if(%client.initialTopHudMenu !$= "newbiehelp")
+		%newtxt = %newtxt @ "</a>";			
+	%newtxt = %newtxt @ " | ";		
+	if(%client.initialTopHudMenu !$= "healthbalance")
+		%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/1>";		
+	%newtxt = %newtxt @ "Health balance";	
+	if(%client.initialTopHudMenu !$= "healthbalance")
+		%newtxt = %newtxt @ "</a>";			
+	%newtxt = %newtxt @ " | ";				
+	if(%client.initialTopHudMenu !$= "nothing")
+		%newtxt = %newtxt @ "<a:cmd SetSetting initialTopHudMenu/2>";		
+	%newtxt = %newtxt @ "Nothing";	
+	if(%client.initialTopHudMenu !$= "nothing")
+		%newtxt = %newtxt @ "</a>";			
+	%newtxt = %newtxt @ "\n\n";				
+	
+	%n = 0;
+	%schemeName[%n] = "";
+	%schemeDesc[%n] = "Based on team"; %n++;
+	%schemeName[%n] = "fr1tz";
+	%schemeDesc[%n] = "fr1tz"; %n++;		
+	%schemeName[%n] = "kurrata";
+	%schemeDesc[%n] = "kurrata"; %n++;		
+	%schemeName[%n] = "c&c";
+	%schemeDesc[%n] = "c&c"; %n++;			
+	%schemeName[%n] = "cga1dark";
+	%schemeDesc[%n] = "CGA #1 Dark"; %n++;		
+	%schemeName[%n] = "cga1light";
+	%schemeDesc[%n] = "CGA #1 Light"; %n++;	
+	
+	%newtxt = %newtxt @ "<lmargin:0>HUD Colors:\n<lmargin:25>";			
+	for(%i = 0; %i < %n; %i++)
+	{
+		if(%client.hudColor !$= %schemeName[%i])
+			%newtxt = %newtxt @ "<a:cmd SetSetting hudColor/" @ %schemeName[%i] @ ">";
+		
+		%newtxt = %newtxt @ %schemeDesc[%i];					
+			
+		if(%client.hudColor !$= %schemeName[%i])
+			%newtxt = %newtxt @ "</a>";				
+
+		%newtxt = %newtxt @ "   ";								
+	}
+	%newtxt = %newtxt @ "\n\n";					
+	
 	%client.addMenuText(%newtxt);
 	%client.endMenuText();
 
