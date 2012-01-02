7cfe92cd4df4454f8a7d4e81915eef2f06892764
Michael Goldener <mg@wasted.ch>
2012-01-02 04:51:10 +0100

    Colorize ROTC: Ethernet game type.

--- game/arenas/rotc-ethernet/common.cs
+++ game/arenas/rotc-ethernet/common.cs
@@ -1,3 +1,3 @@
-$MissionInfo::Type = "ROTC: Ethernet" SPC $GameVersionString;
+$MissionInfo::Type = "\cp\c1ROTC: Ethernet\c2" SPC $GameVersionString @ "\co";
 $MissionInfo::TypeDesc = "Capture all the opposing team's zones to win.";
 $MissionInfo::InitScript = "game/server/missions/rotc-eth.cs";
