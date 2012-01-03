63f8438846a0d34455fbebced335c1abb928fe5b
Michael Goldener <mg@wasted.ch>
2012-01-03 23:51:42 +0100

    Remove redundant code.

--- game/client/scripts/misc.cs
+++ game/client/scripts/misc.cs
@@ -164,6 +164,7 @@ function client_find_sky()
 		if(%obj.getClassName() $= "Sky")
 			return %obj;
 	}
+   return "";
 }
 
 //-----------------------------------------------------------------------------
