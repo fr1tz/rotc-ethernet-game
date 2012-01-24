3e1a98ee6b398cf08980ea50cee5706100924afb
Michael Goldener <mg@wasted.ch>
2012-01-24 17:31:59 +0100

    Add missing files.

---
+++ game/server/weapons/blaster2/blaster2.gfx.red.cs
@@ -0,0 +1,296 @@
+//------------------------------------------------------------------------------
+// Revenge Of The Cats: Ethernet
+// Copyright (C) 2008, mEthLab Interactive
+//------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// laser tail...
+
+datablock LaserBeamData(RedBlaster2ProjectileLaserTail)
+{
+	hasLine = true;
+	lineStartColor	= "1.00 0.00 0.00 0.0";
+	lineBetweenColor = "1.00 0.00 0.00 0.1";
+	lineEndColor	  = "1.00 0.00 0.00 0.2";
+ 	lineWidth		  = 1.0;
+
+	hasInner = true;
+	innerStartColor	= "1.00 0.50 0.50 0.0";
+	innerBetweenColor = "1.00 0.50 0.50 0.2.5";
+	innerEndColor	  = "1.00 0.50 0.50 0.5";
+	innerStartWidth	= "0.0";
+	innerBetweenWidth = "0.05";
+	innerEndWidth	  = "0.1";
+
+	hasOuter = false;
+	outerStartColor	= "1.00 1.00 0.00 0.0";
+	outerBetweenColor = "1.00 1.00 0.00 0.2";
+	outerEndColor	  = "1.00 1.00 0.00 0.2";
+	outerStartWidth	= "0.0";
+	outerBetweenWidth = "0.3";
+	outerEndWidth	  = "0.0";
+
+	//bitmap = "share/textures/rotc/lasertail1.red";
+	//bitmapWidth = 0.1;
+	//crossBitmap = "share/shapes/rotc/weapons/blaster/projectile.impact.green";
+	//crossBitmapWidth = 4.25;
+
+	betweenFactor = 0.5;
+	blendMode = 1;
+};
+
+//-----------------------------------------------------------------------------
+// explosion
+
+datablock ParticleData(RedBlaster2ProjectileExplosion_Cloud)
+{
+	dragCoeffiecient	  = 0.4;
+	gravityCoefficient	= 0;
+	inheritedVelFactor	= 0.025;
+
+	lifetimeMS			  = 600;
+	lifetimeVarianceMS	= 0;
+
+	useInvAlpha = false;
+	spinRandomMin = -200.0;
+	spinRandomMax =  200.0;
+
+	textureName = "share/textures/rotc/corona.png";
+
+	colors[0]	  = "1.0 1.0 1.0 1.0";
+	colors[1]	  = "1.0 1.0 1.0 0.0";
+	colors[2]	  = "1.0 1.0 1.0 0.0";
+	sizes[0]		= 0.5;
+	sizes[1]		= 0.5;
+	sizes[2]		= 0.0;
+	times[0]		= 0.0;
+	times[1]		= 0.2;
+	times[2]		= 1.0;
+
+	allowLighting = true;
+};
+
+datablock ParticleEmitterData(RedBlaster2ProjectileExplosion_CloudEmitter)
+{
+	ejectionPeriodMS = 1;
+	periodVarianceMS = 0;
+
+	ejectionVelocity = 0.25;
+	velocityVariance = 0.25;
+
+	thetaMin			= 0.0;
+	thetaMax			= 90.0;
+
+	lifetimeMS		 = 100;
+
+	particles = "RedBlaster2ProjectileExplosion_Cloud";
+};
+
+datablock ParticleData(RedBlaster2ProjectileExplosion_Dust)
+{
+	dragCoefficient		= 1.0;
+	gravityCoefficient	= -0.01;
+	inheritedVelFactor	= 0.0;
+	constantAcceleration = 0.0;
+	lifetimeMS			  = 1000;
+	lifetimeVarianceMS	= 100;
+	useInvAlpha			 = true;
+	spinRandomMin		  = -90.0;
+	spinRandomMax		  = 500.0;
+	textureName			 = "share/textures/rotc/smoke_particle.png";
+	colors[0]	  = "0.9 0.9 0.9 0.5";
+	colors[1]	  = "0.9 0.9 0.9 0.5";
+	colors[2]	  = "0.9 0.9 0.9 0.0";
+	sizes[0]		= 0.9;
+	sizes[1]		= 1.5;
+	sizes[2]		= 1.6;
+	times[0]		= 0.0;
+	times[1]		= 0.7;
+	times[2]		= 1.0;
+	allowLighting = true;
+};
+
+datablock ParticleEmitterData(RedBlaster2ProjectileExplosion_DustEmitter)
+{
+	ejectionPeriodMS = 10;
+	periodVarianceMS = 0;
+	ejectionVelocity = 2.0;
+	velocityVariance = 0.0;
+	ejectionOffset	= 0.0;
+	thetaMin			= 0;
+	thetaMax			= 180;
+	phiReferenceVel  = 0;
+	phiVariance		= 360;
+	overrideAdvances = false;
+	lifetimeMS		 = 50;
+	particles = "RedBlaster2ProjectileExplosion_Dust";
+};
+
+
+datablock ParticleData(RedBlaster2ProjectileExplosion_Smoke)
+{
+	dragCoeffiecient	  = 0.4;
+	gravityCoefficient	= -0.5;	// rises slowly
+	inheritedVelFactor	= 0.025;
+
+	lifetimeMS			  = 1250;
+	lifetimeVarianceMS	= 0;
+
+	useInvAlpha =  true;
+	spinRandomMin = -200.0;
+	spinRandomMax =  200.0;
+
+	textureName = "share/textures/rotc/smoke_particle.png";
+
+	colors[0]	  = "0.9 0.9 0.9 0.4";
+	colors[1]	  = "0.9 0.9 0.9 0.2";
+	colors[2]	  = "0.9 0.9 0.9 0.0";
+	sizes[0]		= 0.6;
+	sizes[1]		= 2.0;
+	sizes[2]		= 0.6;
+	times[0]		= 0.0;
+	times[1]		= 0.5;
+	times[2]		= 1.0;
+
+	allowLighting = true;
+};
+
+datablock ParticleEmitterData(RedBlaster2ProjectileExplosion_SmokeEmitter)
+{
+	ejectionPeriodMS = 2;
+	periodVarianceMS = 0;
+
+	ejectionVelocity = 2.0;
+	velocityVariance = 0.25;
+
+	thetaMin			= 0.0;
+	thetaMax			= 180.0;
+
+	lifetimeMS		 = 250;
+
+	particles = "RedBlaster2ProjectileExplosion_Smoke";
+};
+
+datablock ParticleData(RedBlaster2ProjectileExplosion_Sparks)
+{
+	dragCoefficient		= 1;
+	gravityCoefficient	= 0.0;
+	inheritedVelFactor	= 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS			  = 500;
+	lifetimeVarianceMS	= 350;
+	textureName			 = "share/textures/rotc/particle1.png";
+	colors[0]	  = "0.56 0.36 0.26 1.0";
+	colors[1]	  = "0.56 0.36 0.26 1.0";
+	colors[2]	  = "1.0 0.36 0.26 0.0";
+	sizes[0]		= 0.5;
+	sizes[1]		= 0.5;
+	sizes[2]		= 0.75;
+	times[0]		= 0.0;
+	times[1]		= 0.5;
+	times[2]		= 1.0;
+	allowLighting = false;
+};
+
+datablock ParticleEmitterData(RedBlaster2ProjectileExplosion_SparksEmitter)
+{
+	ejectionPeriodMS = 10;
+	periodVarianceMS = 0;
+	ejectionVelocity = 4;
+	velocityVariance = 1;
+	ejectionOffset	= 0.0;
+	thetaMin			= 0;
+	thetaMax			= 60;
+	phiReferenceVel  = 0;
+	phiVariance		= 360;
+	overrideAdvances = false;
+	orientParticles  = true;
+	lifetimeMS		 = 100;
+	particles = "RedBlaster2ProjectileExplosion_Sparks";
+};
+
+datablock MultiNodeLaserBeamData(RedBlaster2ProjectileExplosion_Debris_LaserTrail)
+{
+	hasLine = true;
+	lineColor	= "1.00 1.00 0.00 0.5";
+
+	hasInner = false;
+	innerColor = "1.00 1.00 0.00 0.3";
+	innerWidth = "0.20";
+
+	hasOuter = false;
+	outerColor = "1.00 1.00 0.00 0.3";
+	outerWidth = "0.40";
+
+//	bitmap = "share/shapes/rotc/weapons/hegrenade/lasertrail";
+//	bitmapWidth = 0.1;
+
+	blendMode = 1;
+	renderMode = $MultiNodeLaserBeamRenderMode::FaceViewer;
+	fadeTime = 500;
+};
+
+datablock DebrisData(RedBlaster2ProjectileExplosion_Debris)
+{
+//	shapeFile = "share/shapes/rotc/weapons/hegrenade/grenade.dts";
+//	emitters[0] = GrenadeLauncherParticleEmitter;
+
+	laserTrail = RedBlaster2ProjectileExplosion_Debris_LaserTrail;
+
+	// bounce...
+	numBounces = 3;
+	explodeOnMaxBounce = true;
+
+	// physics...
+	gravModifier = 5.0;
+	elasticity = 0.6;
+	friction = 0.1;
+
+	lifetime = 5.0;
+	lifetimeVariance = 0.02;
+};
+
+datablock ExplosionData(RedBlaster2ProjectileExplosion)
+{
+	soundProfile = Blaster2ProjectileExplosionSound;
+
+	lifetimeMS = 200;
+
+ 	// shape...
+	explosionShape = "share/shapes/rotc/effects/explosion2_white.dts";
+	faceViewer	  = false;
+	playSpeed = 8.0;
+	sizes[0] = "0.2 0.2 0.2";
+	sizes[1] = "0.2 0.2 0.2";
+	times[0] = 0.0;
+	times[1] = 1.0;
+
+	//debris = 0; //RedBlaster2ProjectileExplosion_Debris;
+	//debrisThetaMin = 0;
+	//debrisThetaMax = 180;
+	//debrisNum = 3;
+	//debrisVelocity = 50.0;
+	//debrisVelocityVariance = 10.0;
+
+	particleEmitter = RedBlaster2ProjectileExplosion_CloudEmitter;
+	particleDensity = 25;
+	particleRadius = 0.2;
+
+	emitter[0] = RedBlaster2ProjectileExplosion_DustEmitter;
+	emitter[1] = 0; // RedBlaster2ProjectileExplosion_SmokeEmitter;
+	emitter[2] = 0; // RedBlaster2ProjectileExplosion_SparksEmitter;
+
+	// Camera shake
+	shakeCamera = false;
+	camShakeFreq = "10.0 6.0 9.0";
+	camShakeAmp = "20.0 20.0 20.0";
+	camShakeDuration = 0.5;
+	camShakeRadius = 20.0;
+
+	// Dynamic light
+	lightStartRadius = 3;
+	lightEndRadius = 0;
+	lightStartColor = "1.0 0.8 0.2";
+	lightEndColor = "0.0 0.0 0.0";
+};
+
