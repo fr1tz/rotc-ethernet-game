d4b0f88f2de9d31528811d35c23ddc1f4e4c9b72
Michael Goldener <mg@wasted.ch>
2012-03-30 10:41:23 +0200

    Add missing file.

---
+++ game/arenas/rotc-ethernet/eth-archipelago/mission.env.cs
@@ -0,0 +1,24 @@
+// Mission environment script
+// This script is executed from the mission init script in game/server/missions
+
+//------------------------------------------------------------------------------
+// Lights
+//------------------------------------------------------------------------------
+
+$sgLightEditor::lightDBPath = $Server::MissionDirectory @ "lights/";
+$sgLightEditor::filterDBPath = $Server::MissionDirectory @ "filters/";
+sgLoadDataBlocks($sgLightEditor::lightDBPath);
+sgLoadDataBlocks($sgLightEditor::filterDBPath);
+
+//------------------------------------------------------------------------------
+// Material mappings
+//------------------------------------------------------------------------------
+
+%mapping = createMaterialMapping("dark_grey_blue_grid");
+%mapping.sound = $MaterialMapping::Sound::Metal;
+%mapping.color = "0 1 0 1.0 0.0";
+
+%mapping = createMaterialMapping("malloc/dark_blue_grid");
+%mapping.sound = $MaterialMapping::Sound::Metal;
+%mapping.color = "0 1 0 1.0 0.0";
+//%mapping.envmap = "share/textures/malloc/dark_blue_grid 0.5";
